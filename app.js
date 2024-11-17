8606b7a32b37719666c6f179c143616839b45191
Serg <serhiispodyniuk23@gmail.com>
2024-11-17 02:47:45 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+"use strict";
+/*first homework*/ 
+//1
+const x = 10
+const y = 4
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x**y);
+console.log(Math.sqrt(y));
+
+//2
+let myVariable = "42";
+console.log(myVariable);
+
+myVariable = Number(myVariable)
+console.log(myVariable);
+
+myVariable = String(myVariable);
+console.log(myVariable);
+
+myVariable = Boolean(myVariable);
+console.log(myVariable);
