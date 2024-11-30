7b5a7a326fc86f02e973a180cdf72183dca3bcf3
Serg <serhiispodyniuk23@gmail.com>
2024-11-30 11:56:13 +0000

    redone 2 homework and 3 homework

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ let text2 = "";
 //2
 const value = 10;
 let i = 0;
-if (Number.isNaN(value) === false){
+if (Number.isNaN(value)){
  
   for (let a =1;a<value;a++){
     if (a%2 === 0){
@@ -63,5 +63,36 @@ if (Number.isNaN(value) === false){
 
    }
 }
-console.log(text1);
-console.log(text2);
+else {
+    console.log("Таке чуство шо Бог десь наказує нас за шось")
+}
+
+/*third homework*/
+function recursiveOddSumTo(number) {
+    let a = 0;
+        if (number%2 === 0){
+        number -= 1;
+     }
+while (number >= 1){
+
+     a+=number
+   number-=2;
+
+}
+return a;
+};
+
+console.log(recursiveOddSumTo(1)) // 1
+console.log(recursiveOddSumTo(10)) // 25
+
+function iterativeOddSumTo(number) {
+    let a =0;
+for (let b =1;b<=number;b+=2){
+    a+=b
+}
+    return a;
+    
+};
+    
+    console.log(iterativeOddSumTo(1)) // 1
+    console.log(iterativeOddSumTo(10)) // 25
