66090ff1168845941404902d4ed6ea020470431c
Serg <serhiispodyniuk23@gmail.com>
2024-12-03 20:42:25 +0000

    Homewoork2

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
 "use strict";
-/*first homework*/ 
 //1
-const x = 10
-const y = 4
-console.log(x+y);
-console.log(x-y);
-console.log(x*y);
-console.log(x/y);
-console.log(x**y);
-console.log(Math.sqrt(y));
+let text = "";
+for(let i = 1; i<=100;i++){
 
+    if (i%3 === 0 && i%5 === 0 ){
+        text+= " LOLKEK"
+    }
+       else if (i%3 === 0){
+        text+= " KEK"
+    }        
+        else if (i%5 === 0){
+            text+= " LOL"
+
+        }
+        else{ 
+            text+= ` ${i}`}
+}
+console.log(text);
+let text1 = "";
+let text2 = "";
 //2
-let myVariable = "42";
-console.log(myVariable);
+const value = 10;
+let i = 0;
+if (Number.isNaN(value)){
 
-myVariable = Number(myVariable)
-console.log(myVariable);
+  for (let a =1;a<value;a++){
+    if (a%2 === 0){
+        text1 += ` ${a}`;
+    }
 
-myVariable = String(myVariable);
-console.log(myVariable);
+   }
+   while (i<value-2){
+    i+=2;
+    text2 += ` ${i}`;
 
-myVariable = Boolean(myVariable);
-console.log(myVariable);
+   }
+   console.log(text1);
+console.log(text2);
+}
+else {
+    console.log("Таке чуство шо Бог десь наказує нас за шось")
+}
