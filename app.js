35a8809e87163a41786d5491ec3cc95de55d7045
SergSpodyniuk <162581230+SergSpodyniuk@users.noreply.github.com>
2024-12-09 18:15:29 +0000

    Update app.js

--- app.js
+++ app.js
@@ -11,15 +11,8 @@ console.log(Math.sqrt(y));
 
 let myVariable = "42";
 console.log(myVariable);
-     a+=number
-   number-=2;
-
 myVariable = Number(myVariable)
 console.log(myVariable);
-}
-return a;
-};
-
 myVariable = String(myVariable);
 console.log(myVariable);
 myVariable = Boolean(myVariable);
